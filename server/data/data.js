953f96d531ee3f76f7955a14c91638125fd4de68
Venkata Mahesh Polumuri <venkat2552004@gmail.com>
2024-04-24 17:07:17 +0530

    Add mongodb schemas

--- server/data/data.js
+++ server/data/data.js
@@ -1,60 +1,60 @@
 const chats = [
-  {
-    isGroupChat: false,
-    users: [
-      {
-        name: "Hemanth",
-        email: "hemanth@example.com",
-      },
-      {
-        name: "Mahesh",
-        email: "mahesh@example.com",
-      },
-    ],
-    _id: "617a077e18c25468bc7c4dd4",
-    chatName: "Hemanth",
-  },
-  {
-    isGroupChat: false,
-    users: [
-      {
-        name: "Santhosh",
-        email: "santhosh@example.com",
-      },
-      {
-          name: "Mahesh",
-          email: "mahesh@example.com",
-      },
-    ],
-    _id: "617a077e18c25468b27c4dd4",
-    chatName: "Santhosh",
-  },
-  {
-    isGroupChat: true,
-    users: [
-      {
-        name: "Santhosh",
-        email: "santhosh@example.com",
-      },
-      {
-        name: "Mahesh",
-        email: "mahesh@example.com",
-      },
-      {
-        name: "Hemanth",
-        email: "hemanth@example.com",
-      },
-    ],
-    _id: "617a518c4081150716472c78",
-    chatName: "College",
-    groupAdmin: {
-      name: "Mahesh",
-      email: "mahesh@example.com",
-    },
-  }
+	{
+		isGroupChat: false,
+		users: [
+			{
+				name: "Hemanth",
+				email: "hemanth@example.com",
+			},
+			{
+				name: "Mahesh",
+				email: "mahesh@example.com",
+			},
+		],
+		_id: "617a077e18c25468bc7c4dd4",
+		chatName: "Hemanth",
+	},
+	{
+		isGroupChat: false,
+		users: [
+			{
+				name: "Santhosh",
+				email: "santhosh@example.com",
+			},
+			{
+				name: "Mahesh",
+				email: "mahesh@example.com",
+			},
+		],
+		_id: "617a077e18c25468b27c4dd4",
+		chatName: "Santhosh",
+	},
+	{
+		isGroupChat: true,
+		users: [
+			{
+				name: "Santhosh",
+				email: "santhosh@example.com",
+			},
+			{
+				name: "Mahesh",
+				email: "mahesh@example.com",
+			},
+			{
+				name: "Hemanth",
+				email: "hemanth@example.com",
+			},
+		],
+		_id: "617a518c4081150716472c78",
+		chatName: "College",
+		groupAdmin: {
+			name: "Mahesh",
+			email: "mahesh@example.com",
+		},
+	},
 ];
 
 // don't use module.exports = chats ---> we need to export an object so use
 // module.exports = { chats } or module.exports.chats = chats
 
-module.exports = {chats};
+module.exports = { chats };
--- server/server.js
+++ server/server.js
@@ -1,25 +1,22 @@
-const express = require('express')
-const { chats } = require('./data/data')
-require('dotenv').config()
-const app = express()
+const express = require("express");
+const { chats } = require("./data/data");
+require("dotenv").config();
+const app = express();
 
-app.get('/api', (req, res) => {
-    res.send("API is working fine");
+app.get("/api", (req, res) => {
+	res.send("API is working fine");
 });
 
-app.get('/api/chats', (req, res) => {
-    res.send(chats);
+app.get("/api/chats", (req, res) => {
+	res.send(chats);
 });
 
-app.get('/api/chats/:id', (req, res) => {
-    let chat = chats.find((chat) => req.params.id === chat._id);
-    res.send(chat);
+app.get("/api/chats/:id", (req, res) => {
+	let chat = chats.find((chat) => req.params.id === chat._id);
+	res.send(chat);
 });
-
 
 const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
-
-
